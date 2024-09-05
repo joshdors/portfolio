8664deade12d1bfe7bacd6872ad40503d6390b64
joshdors <172191660+jshdors@users.noreply.github.com>
2024-09-04 20:26:43 -0400

    Update links and copy

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,5 +2,5 @@ module.exports = {
     url: process.env.URL || "http://localhost:8080",
     siteName: "Josh Dors",
     siteDescription:
-        "Hi, I'm Josh, a web designer and developer.",
+        "",
 };
